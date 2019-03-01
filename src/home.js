aca0260afb484e5c7fa558a9be9068d2cc804f78
mingliang.sima <mingliang.sima@mobilemd.cn>
2019-03-01 10:40:36 +0800

    新的更改

--- src/home.js
+++ src/home.js
@@ -11,7 +11,7 @@ export default class Home extends React.PureComponent {
             <div>
                 <Button type='primary' onClick={() => this.props.history.push({
                     pathname: '/detail'
-                })}>去玩</Button>
+                })}>go to playing</Button>
             </div>
         );
     }
